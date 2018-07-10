60997e1eb76c6832936eb129e5bc73b5fc865b6e
zhangminglu <1367000465@qq.com>
2018-07-10 13:44:22 +0800

    添加七牛云和云通讯

--- main/create_app.py
+++ main/create_app.py
@@ -1,21 +1,25 @@
 
 from flask import Flask
 
-from app.user_views import user_blueprint
-from .settings import MAIN_CONFIG, STATIC_DIR, TEMPLATES_DIR, start_logging
+from app.blueprint import app_register_blueprint
 from .ext_init import ext_init
+from .settings import MAIN_CONFIG, STATIC_DIR, \
+    TEMPLATES_DIR, start_logging
+from utils.functions import RegexConverter
 
 
 def create_app():
     """ 创建app,注册配置 """
-    # 开始记录日志
-    start_logging()
+    # 开启日志
+    # start_logging()
     # 初始化 app
     app = Flask(__name__, static_folder=STATIC_DIR, template_folder=TEMPLATES_DIR)
-    # 注册路由
-    app.register_blueprint(blueprint=user_blueprint,
-                           url_prefix='/user')
+    # 注册蓝图
+    app_register_blueprint(app=app)
     # 增加配置文件
     app.config.update(MAIN_CONFIG)
+    # 为app中的url路由添加正则表达式匹配
+    # app.url_map.converters['regex'] = RegexConverter
+    # 扩展初始化 app
     ext_init(app=app)
     return app
