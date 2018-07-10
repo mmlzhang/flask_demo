33f7bf2768fff57515e8d79bde1233740141b1e3
zhangminglu <1367000465@qq.com>
2018-07-10 11:08:02 +0800

    添加日志模块

--- main/create_app.py
+++ main/create_app.py
@@ -1,18 +1,18 @@
 
 from flask import Flask
 
-from app.rrc_views import rrc_blueprint
 from app.user_views import user_blueprint
-from .settings import MAIN_CONFIG, STATIC_DIR, TEMPLATES_DIR
+from .settings import MAIN_CONFIG, STATIC_DIR, TEMPLATES_DIR, start_logging
 from .ext_init import ext_init
 
 
 def create_app():
     """ 创建app,注册配置 """
+    # 开始记录日志
+    start_logging()
+    # 初始化 app
     app = Flask(__name__, static_folder=STATIC_DIR, template_folder=TEMPLATES_DIR)
     # 注册路由
-    app.register_blueprint(blueprint=rrc_blueprint,
-                           url_prefix='/rrc')
     app.register_blueprint(blueprint=user_blueprint,
                            url_prefix='/user')
     # 增加配置文件
--- main/settings.py
+++ main/settings.py
@@ -1,5 +1,8 @@
 
 import os
+import datetime
+import logging
+from logging.handlers import RotatingFileHandler
 
 import pymongo
 import redis
@@ -56,3 +59,31 @@ MAIN_CONFIG = {
     'SESSION_REDIS': REDIS_CONN,
     'SESSION_KEY_PREFIX': os.path.split(BASE_DIR)[-1],  # 这里是你的项目的名称
 }
+
+""" 日志配置 """
+LOG_PATH = os.path.join(BASE_DIR, 'logs')
+# 不存在目录就配置目录
+if not os.path.isdir(LOG_PATH):
+    os.mkdir(LOG_PATH)
+# 日志的文件名，以每天的日期作为名称
+LOG_FILE_NAME = datetime.datetime.now().strftime('%Y-%m-%d')
+
+LEVEL = 'DEBUG'  # 调试
+# LEVEL = 'ERROR'  # 错误
+# LEVEL = 'WARNING'  # 警告
+
+
+def start_logging():
+    """日志设置"""
+    # 业务逻辑已开启就加载日志
+    # 设置日志的记录等级
+    logging.basicConfig(level=LEVEL)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/{log_file_name}.log".format(log_file_name=LOG_FILE_NAME),
+                                           maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（应用程序实例app使用的）添加日后记录器
+    logging.getLogger().addHandler(file_log_handler)
